9dbedf8d762048b2db3a95681fd9f6692960297c
VijayGamit665 <vijaygamit665@gmail.com>
2023-03-08 15:33:28 +0530

    added new programm for bubbleSort & SelectionSort & InsertionSort & CountingSort

--- MyDSAJourney/src/com/dsa/Sorting/bubbleSorting.java
+++ MyDSAJourney/src/com/dsa/Sorting/bubbleSorting.java
@@ -7,8 +7,15 @@ import java.util.Optional;
 public class bubbleSorting {
     public static void main(String[] args) {
         int arr[] = {5, 4, 1, 3, 2};
+        int arrs[] = {5, 4, 1, 3, 2, 5, 3, 1};
         bubbleSort(arr);
         printArr(arr);
+        selectionsort(arr);
+        printArr(arr);
+        insertionSort(arr);
+        printArr(arr);
+        contingSort(arrs);
+        printArr(arrs);
 
     }
 
@@ -32,4 +39,58 @@ public class bubbleSorting {
         System.out.println();
     }
 
+    // Time complex city = o(n2)
+    public static void selectionsort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minPos = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minPos] > arr[j]) {
+                    minPos = j;
+                }
+            }
+            // swap
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+
+    // Time complex city = o(n2)
+    public static void insertionSort(int arr[]){
+        for(int i=1; i<arr.length; i++){
+         int curr = arr[i];
+         int prev = i-1;
+
+         // finding out the correct pos to insert
+
+         while (prev >=0 && curr > arr[prev]){
+             arr[prev+1] = arr[prev];
+             prev--;
+         }
+         // insertion
+            arr[prev+1] = curr;
+        }
+    }
+
+    // Time complex city = o(n2)
+    public static void contingSort(int arrs[]){
+        int largest = Integer.MIN_VALUE;
+        for (int i=0; i<arrs.length;i++){
+            largest = Math.max(largest,arrs[i]);
+        }
+        int count[] = new int[largest+1];
+        for (int i=0;i<arrs.length;i++){
+            count[arrs[i]]++;
+        }
+        // Sorting
+        int j=0;
+        for (int i=0; i<count.length;i++){
+            while (count[i]>0){
+                arrs[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
 }
