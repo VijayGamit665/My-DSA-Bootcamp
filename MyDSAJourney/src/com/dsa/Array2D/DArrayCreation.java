dd513bdc5950c17380463d0c8f3ab69a7a9b1385
VijayGamit665 <vijaygamit665@gmail.com>
2023-03-10 01:09:40 +0530

    added new programm for 2DArray Creation

---
+++ MyDSAJourney/src/com/dsa/Array2D/DArrayCreation.java
@@ -0,0 +1,41 @@
+package com.dsa.Array2D;
+
+import java.security.Key;
+import java.util.Scanner;
+
+public class DArrayCreation {
+
+    public static boolean search(int matrix[][], int Key) {
+        // Search for Key
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == Key) {
+                    System.out.println("found at cell (" + i + ", " + j + ")");
+                    return true;
+                }
+            }
+
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int matrix[][] = new int[3][3];
+        int n = matrix.length, m = matrix[0].length;
+        // input from user
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        // out print
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        search(matrix, 4);
+    }
+}
