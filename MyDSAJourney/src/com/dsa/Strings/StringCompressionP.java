5e6df37869d5d96220cda61738991ec7faabe16c
VijayGamit665 <vijaygamit665@gmail.com>
2023-03-21 03:11:03 +0530

    added new programm for String Compression

--- MyDSAJourney/src/com/dsa/Strings/StringCompressionP.java
+++ MyDSAJourney/src/com/dsa/Strings/StringCompressionP.java
@@ -5,6 +5,7 @@ public class StringCompressionP { // Time Complex city = O(n)
     public static void main(String[] args) {
         String str = "aaabbcdd";
         System.out.println(compress(str));
+        System.out.println(compress1(str));
     }
 
     public static String compress(String str) {
@@ -22,4 +23,22 @@ public class StringCompressionP { // Time Complex city = O(n)
         }
         return newStr;
     }
+
+    public static StringBuilder compress1(String str) {
+        StringBuilder newStr = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            Integer count = 1;
+            while (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+                i++;
+            }
+            newStr.append(str.charAt(i));
+            if (count > 1) {
+                newStr.append(count.toString());
+
+            }
+        }
+        return newStr;
+    }
+
 }
