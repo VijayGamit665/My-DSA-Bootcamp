ec695258454cae508f62be3eff1a82aac430fbbd
VijayGamit665 <vijaygamit665@gmail.com>
2023-03-25 10:21:26 +0530

    added new programm for Constructor

---
+++ MyDSAJourney/src/com/dsa/OopsProgram/CopyConstructor.java
@@ -0,0 +1,54 @@
+package com.dsa.OopsProgram;
+
+public class CopyConstructor {
+    public static void main(String[] args) {
+        Std s1 = new Std();
+        //  Std s2 = new Std("Vijay");
+        //  Std s3 = new Std(123);
+        s1.name = "Vijay";
+        s1.Roll = 123;
+        s1.Pass = "xyz";
+        s1.marks[0] = 100;
+        s1.marks[1] = 80;
+        s1.marks[2] = 90;
+
+        Std s2 = new Std(s1);
+        s2.Pass = " XyZA";
+
+        System.out.println(s2.Pass + " " + s2.name + " " + s2.Roll + " ");
+        for (int i = 0; i < 3; i++) {
+            System.out.println(s2.marks[i]);
+        }
+
+    }
+}
+
+class Std {
+    String name;
+    int Roll;
+    String Pass;
+    int marks[];
+
+    Std(Std s1) {
+        marks = new int[3];
+        this.name = s1.name;
+        this.Roll = s1.Roll;
+        this.marks = s1.marks;
+    }
+
+    Std() {
+        marks = new int[3];
+        System.out.println("Conccccc...");
+    }
+
+    Std(String name) {
+        marks = new int[3];
+        this.name = name;
+    }
+
+    Std(int Roll) {
+        marks = new int[3];
+        this.Roll = Roll;
+    }
+}
+
