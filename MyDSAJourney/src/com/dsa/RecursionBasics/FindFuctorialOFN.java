43751bf437a0988ab062a3dd10f036048ad95a37
VijayGamit665 <vijaygamit665@gmail.com>
2023-04-05 20:44:48 +0530

    added new programm for Find Fuctorial OF N

---
+++ MyDSAJourney/src/com/dsa/RecursionBasics/FindFuctorialOFN.java
@@ -0,0 +1,17 @@
+package com.dsa.RecursionBasics;
+
+public class FindFuctorialOFN {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(fact(n));
+    }
+
+    public static int fact(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        int fnm1 = fact(n - 1);
+        int fn = n * fact(n - 1);
+        return fn;
+    }
+}
