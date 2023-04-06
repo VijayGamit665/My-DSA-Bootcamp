e18e8b35707cf737e9ad5d16b39f6db29b17b8e1
VijayGamit665 <vijaygamit665@gmail.com>
2023-04-06 14:41:06 +0530

    added new programm for First Occurrence & LastOccurrence

--- MyDSAJourney/src/com/dsa/RecursionBasics/FirstOccurrence.java
+++ MyDSAJourney/src/com/dsa/RecursionBasics/FirstOccurrence.java
@@ -3,8 +3,9 @@ package com.dsa.RecursionBasics;
 public class FirstOccurrence {
     public static void main(String[] args) {
 
-        int arr[] = {1, 2, 6, 8, 7, 9, 6, 5, 4, 2, 8, 6, 3,};
+        int arr[] = {1, 2, 6, 8, 7, 9, 6, 5, 4, 2, 8, 5, 3,};
         System.out.println(FOccurrence(arr, 5, 0));
+        System.out.println(lOccurrence(arr, 5, 0));
 
     }
 
@@ -18,6 +19,17 @@ public class FirstOccurrence {
         }
 
         return FOccurrence(arr, Key, i + 1);
+    }
+
+    public static int lOccurrence(int arr[], int Key, int i) {
+        if (i == arr.length) {
+            return -1;
+        }
+        int isFound = lOccurrence(arr, Key, i + 1);
+        if (isFound == -1 && arr[i] == Key) {
+            return i;
+        }
+        return isFound;
     }
 
 }
