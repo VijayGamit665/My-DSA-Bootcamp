2162dbe99fc583aee9d1f346c95b6d56bf10e02a
VijayGamit665 <vijaygamit665@gmail.com>
2023-04-14 14:05:16 +0530

    added new programm for Find Permutations

---
+++ MyDSAJourney/src/com/dsa/Backtracking/findPermutations.java
@@ -0,0 +1,26 @@
+package com.dsa.Backtracking;
+
+public class findPermutations {
+    public static void main(String[] args) {
+
+        String str = "abc";
+        findPermutations(str, "");
+
+    }
+
+    public static void findPermutations(String str, String ans) {
+
+        // base case
+        if (str.length() == 0) {
+            System.out.println(ans);
+            return;
+        }
+
+        // recursion
+        for (int i = 0; i < str.length(); i++) {
+            char curr = str.charAt(i);
+            String newStr = str.substring(0, i) + str.substring(i + 1);
+            findPermutations(newStr, ans + curr);
+        }
+    }
+}
