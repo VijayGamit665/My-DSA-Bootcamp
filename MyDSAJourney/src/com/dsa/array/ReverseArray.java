49e7813296dcf194495c1d9d2d3a41d110005843
VijayGamit665 <vijaygamit665@gmail.com>
2023-04-21 16:10:49 +0530

    added new Program for Reverse Array

--- MyDSAJourney/src/com/dsa/array/ReverseArray.java
+++ MyDSAJourney/src/com/dsa/array/ReverseArray.java
@@ -4,28 +4,29 @@ import java.util.Arrays;
 
 //Tc : 0(n) or o(n/2)
 //sc : 0(1)
-public class RevarseArray {
+public class ReverseArray {
     public static void main(String[] args) {
         int numbers[] = {1, 2, 3, 6, 5, 8, 9, 10};
-        reverseArrray(numbers);
+        reverseArray(numbers);
 
         //for print reverse array
 //      System.out.println(Arrays.toString(numbers));
 
-        for(int n : numbers){//enhance for loop
-            System.out.print(n +" ");
+        for (int n : numbers) {//enhance for loop
+            System.out.print(n + " ");
         }
 //        Arrays.stream(numbers).forEach(n-> System.out.println(n)); // foreach loop ,jav8
 //        Arrays.stream(numbers).forEach(System.out::println); //using :: operator or refferance
 
 //        Arrays.stream(numbers).filter(a-> a%2==0).forEach(n-> System.out.println(n)); // find even num by using filter java8
     }
-    public static void reverseArrray(int[] arr){
-        int start=0,end=arr.length-1;
+
+    public static void reverseArray(int[] arr) {
+        int start = 0, end = arr.length - 1;
 
 
         //with while loop
-        while (start < end ){
+        while (start < end) {
             int temp = arr[end];
             arr[end] = arr[start];
             arr[start] = temp;
